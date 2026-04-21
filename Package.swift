735c4f4ab6a2b17f21d7ceb52a6edb97e28da1b9
pfriedrix <pfriedrix@icloud.com>
2026-04-22 00:20:11 +0300

    chore: consolidate to single Package.swift with Swift 6 mode
    Drop the Package@swift-6.0.swift backward-compat variant and require
    Swift 6 as the minimum toolchain. Bump tools-version to 6.0 and enable
    swiftLanguageModes: [.v6] at the package level.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import CompilerPluginSupport
 import PackageDescription
@@ -40,4 +40,5 @@ let package = Package(
             ]
         )
     ],
+    swiftLanguageModes: [.v6]
 )
